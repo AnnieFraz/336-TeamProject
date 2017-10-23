66337f30dedd296d0cba89eb27b94f3cecd008a4
Anna Rasburn <annarasburn97@gmail.com>
2017-10-23 05:45:58 +0000

    Annas work so far, Includes a CSS file and start to the filter work

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+        $dbHost = getenv('IP');
+        $dbPort = 3306;
+        $dbName = "team_project";
+        $username = "anniefraz";
+        $password = "";
+        
+        $dbConn = new PDO("mysql:host=$dbHost;port=$dbPort;dbname=$dbName", $username, $password);
+        $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+?>
+<html>
+        <head>
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+        </head>
+        
+        <body>
+            <h1>Playlist Database</h1>
+            <form method="get">
+          	  Search for: <input type="text" name="filter_criteria" maxlength="15" value="Enter Here"/>
+          	  Filter by: 
+          	   <select name="choice">
+          	   	  <option value="song"> Song </option>
+          	   	  <option value="artist"> Artist </option>
+          	   	  <option value="playlist"> Playlist </option>
+          	   </select>
+          	   <center><input type='submit' value='Send' name='submit'</center>
+          	   </form>
+         <?php
+         $filter_criteria = (isset($_POST['filter_criteria']) ? $_POST['filter_criteria'] : null);
+         $sql = "";
+         
+         //if ($_POST['choice'] == 'song') {
+                // $sql = "SELECT * FROM song WHERE song_title= " .$filter_criteria;
+        // }
+        // if ($_POST['choice'] == 'artist') {
+                // $sql = "SELECT * FROM song WHERE song_artist= " .$filter_criteria;
+        // }
+        // if ($_POST['choice'] == 'playlist') {
+                // $sql = "SELECT * FROM playlist WHERE playlist_name= " .$filter_criteria;
+         //}
+         
+          $sql = "SELECT * FROM songs WHERE song_title=".mysql_real_escape_string($_POST['filter_criteria'] . ";");
+          $query = mysql_query("SELECT * FROM songs WHERE song_title=" . mysql_real_escape_string($_POST['filter_criteria']));
+         
+         $stmt = $dbConn->prepare($query);
+         $stmt->execute();
+         while($row=$stmt->fetch()){
+         echo "<tr>
+        <td>{$row['song_artist']}</td>
+        </tr>";
+}
+         ?>
+        </body>
+        </html>
